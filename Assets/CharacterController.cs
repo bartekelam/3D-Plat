8249ff3b8cbb48b7fb922bb290341c2e30b77d07
bartekelam <113114516+bartekelam@users.noreply.github.com>
2022-12-02 12:52:17 +0000

    j6trkytyk

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,40 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class CharacterController : MonoBehaviour
+
 {
-    float maxSpeed = 20.0f;
-    float rotation = 0.0f;
-    float camRotation = 0.0f;
+
+    Animator myAnim;
+
+    void Start()
+    {
+        myAnim = GetComponentInChildren<Animator>();
+
+        cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public float maxSpeed;
+    public float normalSpeed = 4.0f;
+    public float sprintSpeed = 8.0f;
+    public float rotation = 0.0f;
+    public float camRotation = 0.0f;
+    public float rotationSpeed = 2.0f;
+    public float camRotationSpeed = 1.5f;
     GameObject cam;
     Rigidbody myRigidbody;
 
     bool isOnGround;
     public GameObject groundChecker;
     public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        cam = GameObject.Find("Main Camera");
-        myRigidbody = GetComponent<Rigidbody>();
-    }
-    float rotationSpeed = 2.0f;
-    float camRotationSpeed = 1.5f;
-    // Update is called once per frame
     void Update()
     {
-        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
-
         isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        myAnim.SetBool("isOnGround", isOnGround);
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnim.SetTrigger("jumped");
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = sprintSpeed;
+        } else
+        {
+            maxSpeed = normalSpeed;
+        }
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        myAnim.SetFloat("speed", newVelocity.magnitude);
+
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
-        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = camRotation - Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = Mathf.Clamp(camRotation, -10.0f, 30.0f);
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
     }
 }
